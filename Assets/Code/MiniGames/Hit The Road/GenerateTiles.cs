148da01fda8c1fc81be29c679e441c447a43d788
unknown <mlabady@gamingcampus.local>
2025-04-03 13:01:23 +0200

    creation de prefab du terrain
    instatiation du prefab
    
     a faire : faire spawn le prefab dans le champs de vision de la camera et le faire defiler, puis le detruire lorsqu'il est en dehors de la camera

---
+++ Assets/Code/MiniGames/Hit The Road/GenerateTiles.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTiles : MonoBehaviour
+{
+    public GameObject tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (tile != null)
+        {
+            Instantiate(tile, new Vector3(0,0,0), Quaternion.identity);    
+        }
+        else
+        {
+            Debug.Log("Erreur variable Tile non assigné !");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
