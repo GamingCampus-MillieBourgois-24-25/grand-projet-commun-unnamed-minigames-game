834915121d96b48c9f2991ae20fb0211b5ffb7b3
Mr-Sim <simoncatanese.pro@gmail.com>
2025-04-03 12:57:48 +0200

    base

---
+++ Assets/Code/GLOBAL/SceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Code.GLOBAL
+{
+    public class SceneController : MonoBehaviour
+    {
+        // Ctrl + M + O pour déplier toutes les régions
+        #region PROPERTIES----------------------------------------------------------------------
+
+        private string loaded
+
+        #endregion
+        #region LIFECYCLE-----------------------------------------------------------------------
+
+
+
+        #endregion
+        #region METHODS-------------------------------------------------------------------------
+
+
+
+        #endregion
+        #region API-----------------------------------------------------------------------------
+
+        public static bool ChangeScene(string sceneName)
+        {
+            return false;
+        }
+
+        #endregion
+        #region COROUTINES----------------------------------------------------------------------
+
+
+
+
+        #endregion
+    }
+}
