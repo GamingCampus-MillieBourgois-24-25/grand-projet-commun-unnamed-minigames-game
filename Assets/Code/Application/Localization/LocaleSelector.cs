aaf9bbdca57b0c008b77a19fef85ac20fb6f9992
Horaclee <nael.bellonaitoussekri@gmail.com>
2025-04-07 15:15:16 +0200

    update language selector

--- Assets/Code/Application/Localization/LocaleSelector.cs
+++ Assets/Code/Application/Localization/LocaleSelector.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
 
 namespace Axoloop.Application.Option
 {
     public class LocaleSelector : MonoBehaviour
     {
         private bool _active = false;
+
+        public Button englishButtonLanguage;
+        public Button frenchButtonLanguage;
+        public Sprite englishOn;
+        public Sprite englishOff;
+        public Sprite frenchOn;
+        public Sprite frenchOff;
+        
 
         private void Start()
         {
@@ -18,6 +27,25 @@ namespace Axoloop.Application.Option
         public void ChangeLocale(int localeID)
         {
             if (_active == true) return;
+
+            if (localeID == 0)
+            {
+                englishButtonLanguage.image.sprite = englishOn;
+                frenchButtonLanguage.image.sprite = frenchOff;
+
+                englishButtonLanguage.interactable = false;
+                frenchButtonLanguage.interactable = true;
+            }
+
+            if (localeID == 1)
+            {
+                englishButtonLanguage.image.sprite = englishOff;
+                frenchButtonLanguage.image.sprite = frenchOn;
+                
+                englishButtonLanguage.interactable = true;
+                frenchButtonLanguage.interactable = false;
+            }
+            
             StartCoroutine(SetLocale(localeID));
         }
         
