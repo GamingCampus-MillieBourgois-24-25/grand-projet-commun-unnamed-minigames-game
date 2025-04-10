0cbefffbc5e8f087524cd758fefae41ef9b1858c
Naox <erosalie@gaming.tech>
2025-04-10 11:47:14 +0200

    [ADD] Ajout de la rotation de la roue

---
+++ Assets/Application/MainScreens/Scripts/WheelRotator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class WheelRotatorMobile : MonoBehaviour
+{
+    public float rotationSpeed = 0.5f;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // On ne s'intéresse qu'au déplacement (pas au tap ou au début/fin du toucher)
+            if (touch.phase == TouchPhase.Moved)
+            {
+                float deltaX = touch.deltaPosition.x;
+                transform.Rotate(0, 0, -deltaX * rotationSpeed);
+            }
+        }
+    }
+}
+
